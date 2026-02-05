11daf37ae3b9a69efa49e3fd8f5b24af6e71c7ca
James White <james_white249@mymail.eku.edu>
2026-02-05 11:24:51 -0500

    Initial Commit

---
+++ M3Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M3Weather
+//
+//  Created by White, James B. on 2/5/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
